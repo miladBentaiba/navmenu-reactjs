4f7987ac47ca07868ad63f64259ecb3119e9de24
miladBentaiba <bm_bentaiba@esi.dz>
2019-06-10 20:09:20 +0100

    active now works correctly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Link visitedClassName='is-active' to="/">Home</Link>&nbsp;
-      <Link visitedClassName='is-active' to="/about">About</Link>&nbsp;
-      <Link visitedClassName='is-active' to="/topics">Topics</Link>&nbsp;
+      <NavLink activeStyle={{ backgroundColor: 'yellow' }} to="/home" onlyActiveOnIndex>Home</NavLink>&nbsp;
+      <NavLink activeStyle={{ backgroundColor: 'yellow' }} to="/about">About</NavLink>&nbsp;
+      <NavLink activeStyle={{ backgroundColor: 'yellow' }} to="/topics">Topics</NavLink>&nbsp;
       <hr />
-      <Route exact path="/" render={() => (<Home text='Home' />)} />
+      <Route exact path="/home" render={() => (<Home text='Home' />)} />
       <Route path="/about" render={() => (<Home text='About' />)} />
       <Route path="/topics" component={Topics} />
     </BrowserRouter>
@@ -23,9 +23,9 @@ const Topic = ({ match }) => <h3>{match.params.id}</h3>
 const Topics = ({ match }) =>
   <div>
     <h2>Topics</h2>
-    <Link to={`${match.url}/rendering`}>{`${match.url}/rendering`}</Link><br />
-    <Link to={`${match.url}/components`}>{`${match.url}/components`}</Link><br />
-    <Link to={`${match.url}/props-v-state`}>{`${match.url}/props-v-state`}</Link><br />
+    <NavLink activeStyle={{ backgroundColor: 'yellow' }} to={`${match.url}/rendering`}>{`${match.url}/rendering`}</NavLink><br />
+    <NavLink activeStyle={{ backgroundColor: 'yellow' }} to={`${match.url}/components`}>{`${match.url}/components`}</NavLink><br />
+    <NavLink activeStyle={{ backgroundColor: 'yellow' }} to={`${match.url}/props-v-state`}>{`${match.url}/props-v-state`}</NavLink><br />
 
     <Route path={`${match.path}/:id`} component={Topic} />
     <Route exact path={match.path} render={() => <h3>Please select a topic.</h3>} />
